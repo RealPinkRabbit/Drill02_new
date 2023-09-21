3a1139e021815c189936bf21838fcc5db230b147
RealPinkRabbit <sdg0896@gmail.com>
2023-09-21 10:23:02 +0900

    네모운동 첫번째 줄 그리기

--- character_circle_rectangle.py
+++ character_circle_rectangle.py
@@ -12,9 +12,6 @@ character = load_image('character.png')
 def run_circle():
     print('CIRCLE')
 
-    # 일단 그림을 그리자
-    
-
     # 원 회전
     cx, cy, r = 400, 300, 200
     for deg in range(0,360,1):
@@ -23,15 +20,18 @@ def run_circle():
         clear_canvas_now()
         grass.draw_now(400, 30)
         character.draw_now(x, y)
-        delay(0.01)    
-    pass
+        delay(0.01) 
 
 def run_rectangle():
     print('RECTANGLE')
-    pass
+    for x in range(50,750+1, 10):
+        clear_canvas_now()
+        grass.draw_now(400, 30)
+        character.draw_now(x, 90)
+        delay(0.01)
 
 while True:
-    run_circle()
+    # run_circle()
     run_rectangle()
     break;
 
@@ -42,3 +42,10 @@ close_canvas()
     # Tip. 가장 먼저 뼈대라도 실행이 되는 프로그램을 만든다 (pass 이용)
     # ① 무한반복을 만든다
     # ② 쓸 함수를 만들어 놓고 나서 선언하러 간다
+
+# Tip. while문보다는 for문을 쓰는 것을 추천
+# Tip. 매개변수는 가능한 변수 1개로
+# Tip. 함수 1개 만들고 나서 정상동작 확인되면, main문에서 주석처리
+    # "test read time"을 줄이기 위함
+    
+
